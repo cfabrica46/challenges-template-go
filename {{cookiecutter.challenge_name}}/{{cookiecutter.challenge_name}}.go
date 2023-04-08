ce2bc8881daf2631f11ad468d145de66e791edef
César Caycho <cfabrica46@gmail.com>
2023-04-07 22:23:50 -0500

    feat: set in a var

--- {{cookiecutter.challenge_name}}/{{cookiecutter.challenge_name}}.go
+++ {{cookiecutter.challenge_name}}/{{cookiecutter.challenge_name}}.go
@@ -1,5 +1,5 @@
 package {{cookiecutter.challenge_name}}
 
-func {{cookiecutter.challenge_name.replace('-',' ').title().replace(' ', '')}}(int32) int32 {
+func {{cookiecutter.challenge_name_func}}(int32) int32 {
 	return 0
 }
--- {{cookiecutter.challenge_name}}/{{cookiecutter.challenge_name}}_test.go
+++ {{cookiecutter.challenge_name}}/{{cookiecutter.challenge_name}}_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func Test{{cookiecutter.challenge_name.replace('-',' ').title().replace(' ', '')}}(t *testing.T) {
+func Test{{cookiecutter.challenge_name_func}}(t *testing.T) {
 	t.Parallel()
 
 	for _, tt := range []struct {
